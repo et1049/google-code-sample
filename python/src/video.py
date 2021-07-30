3ef9d1dbf15b28abd6006d74154a9b2327b65298
et1049 <88098081+et1049@users.noreply.github.com>
2021-07-30 12:56:50 +0100

    Created playlist properties.

--- python/src/video.py
+++ python/src/video.py
@@ -29,3 +29,24 @@ class Video:
     def tags(self) -> Sequence[str]:
         """Returns the list of tags of a video."""
         return self._tags
+
+class Playlist:
+    """A class used to represent a playlist."""
+
+    def __init__(self, play_title: str, playlist_items: Sequence[str]):
+        """Video constructor."""
+        self._play_title = play_title
+
+        # Turn the songs into a tuple here so it's unmodifiable,
+        # in case the caller changes the 'playlist_songs' they passed to us
+        self._items = tuple(playlist_items)
+
+    @property
+    def play_title(self) -> str:
+        """Returns the title of a playlist."""
+        return self._play_title
+
+    @property
+    def items(self) -> Sequence[str]:
+        """Returns the list of songs of a playlist."""
+        return self._items
